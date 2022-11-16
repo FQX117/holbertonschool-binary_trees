458e06fd8261ef5b7ee98ed1099c52cff6f00858
FQX117 <blackfox950@gmail.com>
2022-11-16 11:03:07 -0600

    add typedef

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 /* Our functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
